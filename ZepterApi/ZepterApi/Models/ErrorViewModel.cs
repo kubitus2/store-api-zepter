c5e9276b91b45d3073e29be06d1e481d1bc6f306
kubitus2 <jakubosinski2@gmail.com>
2025-01-17 16:16:04 +0100

    Initial commit.

---
+++ ZepterApi/ZepterApi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ZepterApi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
